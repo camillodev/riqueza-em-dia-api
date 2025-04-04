6344302d069871b752f4c7be7b371919b6b95da3
rafaelcamilloiollo <camillodeveloper@gmail.com>
2025-04-04 16:46:05 -0300

    Remove unused ConfigModule and global exception filter from AppModule, simplifying the module structure.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -8,26 +6,16 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AccountsModule } from './accounts/accounts.module';
 import { CommonModule } from './common/common.module';
-import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 @Module({
   imports: [
     CommonModule,
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
     PrismaModule,
     UsersModule,
     AuthModule,
     AccountsModule,
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_FILTER,
-      useClass: AllExceptionsFilter,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule { }
